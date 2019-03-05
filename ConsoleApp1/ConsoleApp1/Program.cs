709244b4969de6300710313c91a40605e1eb55e0
Dmitry <lbvfyf2@mail.ru>
2019-03-05 12:57:40 +0300

    slianie

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,22 +17,6 @@ namespace ConsoleApp1
             ConsoleKeyInfo key = Console.ReadKey();
 
             char ch = key.KeyChar;
-
-
-
-            switch (ch)
-
-            {
-
-                case '+':
-
-                    A = A + B;
-
-                    break;
-
-                default:
-
-                    break;
-            }
+        }
     }
 }
