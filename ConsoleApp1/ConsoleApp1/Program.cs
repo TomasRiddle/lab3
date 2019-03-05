fce962011278273a9dec4a56804d505d4ac82e2b
Dmitry <lbvfyf2@mail.ru>
2019-03-05 12:07:21 +0300

    first khm save

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+
+            int B = int.Parse(Console.ReadLine());
+
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            char ch = key.KeyChar;
+        }
+    }
+}
